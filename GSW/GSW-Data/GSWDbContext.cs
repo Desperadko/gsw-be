7a819221d36e073058fd83c028192ce82ceea885
Desperadko <drakostvbg@gmail.com>
2025-10-06 17:25:05 +0300

    Add missing directives/dependencies | Forgot to invoke the AddServices method

--- GSW/GSW-Data/GSWDbContext.cs
+++ GSW/GSW-Data/GSWDbContext.cs
@@ -5,7 +5,6 @@ namespace GSW_Data
 {
     public sealed class GSWDbContext : DbContext
     {
-        public GSWDbContext() { }
         public GSWDbContext(DbContextOptions<GSWDbContext> options) : base(options) { }
 
         public DbSet<TestModel> Tests { get; set; } = null!;
